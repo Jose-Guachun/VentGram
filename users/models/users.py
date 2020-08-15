f14eadbcea8bb8fefb8100028e298b3ed25cf6e0
Jose Guachun <joseguachuns@hotmail.com>
2020-08-15 15:46:43 -0500

    modelo de usuario con abtract user funcional solo en el admin

--- users/models/users.py
+++ users/models/users.py
@@ -1,5 +1,8 @@
 #dependencias de django
-from django.contrib.auth.models import AbstractUser
+from __future__ import unicode_literals
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.conf import settings
 
@@ -18,11 +21,6 @@ class User(CRideModel ,AbstractUser):
             'unique': 'Correo electronico en uso.'
         }
         )
-    code=models.CharField(max_length=20)
-    
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=['username', 'first_name', 'last_name', ]
-
     is_client=models.BooleanField(
         'client status',
         default=True,
@@ -38,8 +36,17 @@ class User(CRideModel ,AbstractUser):
         help_text='se establece en verdadero cuando el usuario ha verificado su dirección de correo electrónico',
     )
 
+    code=models.CharField(max_length=20)
+
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username', 'first_name', 'last_name', ]
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
     def __str__ (self):
         return self.username
+        
     
-    def get_short_name(self):
-        return self.username
