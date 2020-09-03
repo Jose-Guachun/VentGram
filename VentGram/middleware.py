e2ad82b512eb6a109e7f6e761d4de25df5ebfd45
Jose Guachun <joseguachuns@hotmail.com>
2020-09-02 22:41:26 -0500

    se bloqueo el signup y home cuando se inicia sesion con middleware

---
+++ VentGram/middleware.py
@@ -0,0 +1,25 @@
+1#platzigram middleware
+
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class SignUpBlockMiddleware:
+    #Middleware de finalización de perfil.
+    # Asegúrese de que cada usuario que interactúa con la plataformatienen su foto de perfil y biografía.
+    def __init__(self, get_response):
+        """inicializacion del middleware"""
+        self.get_response =get_response
+    
+
+    def __call__(self, request):
+        """Código a ejecutar para cada solicitud antes de que se llame a 
+            la vista."""
+        if not request.user.is_anonymous:
+            if  not request.user.is_staff:
+                    if request.path in [reverse('users:signup'), reverse('posts:home')]:
+                        return redirect('posts:feed')
+
+        response = self.get_response(request)
+        return response
+
+
--- VentGram/settings.py
+++ VentGram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #LocalApp
     'users',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'VentGram.middleware.SignUpBlockMiddleware',
 ]
 
 ROOT_URLCONF = 'VentGram.urls'
