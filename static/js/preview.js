f060043aba2973c65d7d969b2de53e481cb36ec9
JosePcWindows <joseguachuns@hotmail.com>
2021-05-19 13:35:31 -0500

    se valido file de imagen y documento js

--- static/js/preview.js
+++ static/js/preview.js
@@ -1,11 +1,18 @@
 function readImage(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $('#preview').attr('src', e.target.result); // Renderizamos la imagen
-    }
-    reader.readAsDataURL(input.files[0]);
-  }
+  var archivoInput = document.getElementById("file");
+  var archivoRuta = archivoInput.value;
+  var extPermitidas = /(.jpg|.jpeg)$/i;
+  if(!extPermitidas.exec(archivoRuta)){
+      alert('Asegurese de haber seleccionado una imagen');
+      archivoInput.value = '';
+      return false;
+    } else if ((input.files && input.files[0])) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#preview').attr('src', e.target.result); // Renderizamos la imagen
+      }
+      reader.readAsDataURL(input.files[0]);  
+  }  
 }
 
 $("#file").change(function () {
@@ -18,19 +25,30 @@ window.addEventListener('load', inicio, false);
 
 function inicio() {
   var archivo = document.getElementById("archivo");
-  if(archivo)
-  {
-    document.getElementById('archivo').addEventListener('change', cargar, false);
-  }
   
-}
+  if(archivo)
+    {
+      document.getElementById('archivo').addEventListener('change', cargar, false);
+      }
+    }
+
 
 function cargar(ev) {
-  document.getElementById('datos').innerHTML = ev.target.files[0].name
+  var archivoInput = document.getElementById("archivo");
+  var archivoRuta = archivoInput.value;
+  var extPermitidas = /(.pdf)$/i;
+  if(!extPermitidas.exec(archivoRuta)){
+      alert('Asegurese de haber seleccionado un PDF');
+      archivoInput.value = '';
+      return false;
+    }else{
+      document.getElementById('datos').innerHTML = ev.target.files[0].name
 
-  var arch = new FileReader();
-  arch.addEventListener('load', leer, false);
-  arch.readAsText(ev.target.files[0]);
+      var arch = new FileReader();
+      arch.addEventListener('load', leer, false);
+      arch.readAsText(ev.target.files[0]);
+    }
+ 
 }
 
 function leer(ev) {
@@ -42,3 +60,4 @@ function leer(ev) {
   
 }
 //reflejar documento 
+
